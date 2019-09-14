2db56d043d40db2b99284b9cc749719dbe2469a2
care0717 <dev.care0717@gmail.com>
2019-09-14 18:55:30 +0900

    fix: typo

--- app/cafe/CafeRepositoryImpl.scala
+++ app/cafe/CafeRepositoryImpl.scala
@@ -12,24 +12,27 @@ class CafeRepositoryImpl @Inject()(
                                     implicit ec: ExecutionContext
                                   ) extends CafeRepository {
   implicit val session = AutoSession
-  def mkCafeEntity(rs: WrappedResultSet) = Cafe(
-    id = rs.get("id"),
-    name = rs.get("name"),
-    coodinate = Coodinate(rs.get("lattiude"), rs.get("longtitude")),
-    rating = Rating(rs.bigDecimalOpt("averaged_value").map(BigDecimal(_))),
-    images  = rs.stringOpt("urls").map(_.split(",").map(Image).toSeq).getOrElse(Seq[Image]())
-  )
+  def mkCafeEntity(rs: WrappedResultSet): Cafe =
+    Cafe(
+      id = rs.get("id"),
+      name = rs.get("name"),
+      coodinate = Coodinate(rs.get("latitude"), rs.get("longitude")),
+      rating = Rating(rs.bigDecimalOpt("averaged_value").map(BigDecimal(_))),
+      images = rs.stringOpt("urls").map(_.split(",").map(Image).toSeq).getOrElse(Seq[Image]())
+    )
 
   override def findAll(): Future[Seq[Cafe]] =
     Future {
       sql"""
-        select c.cafe_id as id, c.lattiude as lattiude, c.longtitude as longtitude, c.name as name, group_concat(distinct i.url) as urls, avg(r.value) as averaged_value
-          from cafes as c
-            left join images as i
-              using (cafe_id)
-            left join ratings as r
-              using (cafe_id)
-            group by c.cafe_id;
+        select
+          c.cafe_id as id, c.latitude as latitude, c.longitude as longitude, c.name as name,
+          group_concat(distinct i.url) as urls, avg(r.value) as averaged_value
+        from cafes as c
+        left join images as i
+          using (cafe_id)
+        left join ratings as r
+          using (cafe_id)
+        group by c.cafe_id;
       """
         .map(rs => mkCafeEntity(rs))
         .list
