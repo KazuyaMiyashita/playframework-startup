1bc9bb5d01d06f6353ca96f063f70c5c328da9e8
KazuyaMiayshita <miy1924@gmail.com>
2019-10-20 22:29:05 +0900

    wip 何もかもがわからない

--- app/application/auth/LoginController.scala
+++ app/application/auth/LoginController.scala
@@ -2,17 +2,17 @@ package application.auth
 
 import javax.inject.{Singleton, Inject}
 import play.api.mvc._
-import utils.FormUtils.bindFromRequest
 import scala.concurrent.Future
 import cats.data.EitherT
 import cats.implicits._
-
-import utils.CirceWritable._
+import domain.auth.UserAuthorizer
+import application.utils.FormUtils.bindFromRequest
+import application.utils.CirceWritable._
 
 @Singleton
 class LoginController @Inject()(
   cc: ControllerComponents,
-  userRepository: UserRepository,
+  userAuthorizer: UserAuthorizer[Future],
 ) extends AbstractController(cc) {
 
   implicit val ec = cc.executionContext
@@ -27,7 +27,7 @@ class LoginController @Inject()(
     }
 
     def login(form: LoginForm): Future[Either[Result, Result]] = {
-      userRepository.login(form.email, form.rawPassword).map { tokenOpt =>
+      userAuthorizer.login(form.email, form.rawPassword).map { tokenOpt =>
         tokenOpt match {
           case None => Left(BadRequest)
           case Some(token) => Right(Ok(LoginResponse(token).json))
